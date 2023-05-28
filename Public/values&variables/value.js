7a32b47049e3508d7e7892af9ee55551d59c55ed
tolujacob05 <tolujacob05@gmail.com>
2023-05-28 02:40:30 +0100

    part one done

--- Public/values&variables/value.js
+++ Public/values&variables/value.js
@@ -112,3 +112,134 @@ const percentages = [
 ];
 
 console.log(percentages);
+console.log(typeof populations);
+
+let y = 20 + 13;
+y += 10;
+console.log(y);
+
+const markWeight = 78;
+const markHeight = 1.69;
+const johnWeight = 92;
+const johnHeight = 1.95;
+
+function bmi(markHeight, markWeight) {
+  return markWeight / markHeight ** 2;
+}
+
+function bmi2(johnHeight, johnWeight) {
+  return johnWeight / johnHeight ** 2;
+}
+
+const markBmi = bmi(1.69, 78);
+console.log(markBmi);
+
+const johnBmi = bmi2(1.95, 92);
+console.log(johnBmi);
+
+console.log(markBmi > johnBmi);
+
+// OR
+
+const weightMark = 95;
+const heightMark = 1.88;
+const weightJohn = 85;
+const heightJohn = 1.76;
+
+const bmiMark = weightMark / heightMark ** 2;
+const bmiJohn = weightJohn / heightJohn ** 2;
+
+console.log(bmiMark, bmiJohn);
+// console.log(bmiMark > bmiJohn);
+
+const name1 = "Mark";
+const name2 = "John";
+
+if (bmiMark > bmiJohn) {
+  console.log(
+    `${name1}'s BMI ${bmiMark} is heigher than ${name2}'s ${bmiJohn}`
+  );
+} else {
+  console.log(
+    `${name2}'s BMI ${bmiMark} is heigher than ${name1}'s ${bmiJohn}`
+  );
+}
+
+const team1 = "dolphins";
+const team2 = "koalas";
+const dolphins = (96 + 108 + 89) / 3;
+const koalas = (88 + 91 + 110) / 3;
+
+console.log(dolphins, koalas);
+
+// const average = (dolphins + koalas) / 2;
+
+// console.log(average);
+
+if (dolphins > koalas) {
+  console.log(`${team1} won the competition`);
+} else {
+  console.log(`${team2} won the competition`);
+}
+
+const teamDolp = (97 + 112 + 101) / 2;
+const teamKoalas = (109 + 95 + 123) / 2;
+console.log(teamDolp, teamKoalas);
+
+if (teamKoalas >= 100 && teamDolp >= 100 && teamKoalas > teamDolp) {
+  console.log("team koalas won");
+} else if (teamDolp >= 100 && teamKoalas >= 100 && teamDolp > teamKoalas) {
+  console.log("team dolphins won");
+}
+
+const dolphin2 = (97 + 112 + 101) / 3;
+const koalas2 = (109 + 95 + 106) / 3;
+// const minimumScore = 100;
+console.log(dolphin2, koalas2);
+
+if (dolphin2 === koalas2) {
+  console.log("match ends with a draw");
+}
+
+const day = "frid";
+
+if (day === "monday") {
+  console.log("I must go to work");
+} else if (day === "tuesday") {
+  console.log("Executing already written codes");
+} else if (day === "wednesday" || day === "thursday") {
+  console.log("Champions league and Europa league match days");
+} else if (day === "friday") {
+  console.log("YAY! TGIF");
+} else if (day === "saturday" || day === "sunday") {
+  console.log("Premier league day");
+} else {
+  console.log("Please input a valid day of the week");
+}
+
+const tip = (15 / 100) * 275;
+console.log(tip);
+
+const bill = 275;
+
+const total = bill + tip;
+console.log(total);
+
+bill === 275
+  ? console.log(
+      `the bill was ${bill}, the tip is ${tip}, and the total value is ${total}`
+    )
+  : "say something";
+
+// const tip2 = (20 / 100) * 40;
+// console.log(tip2);
+
+// OR
+const bill2 = 430;
+const tip2 =
+  bill2 <= 300 && bill2 >= 50 ? bill2 * (15 / 100) : bill2 * (20 / 100);
+console.log(
+  `the bill was ${bill2}, the tip is ${tip2}, and the total value is ${
+    tip2 + bill2
+  }`
+);
